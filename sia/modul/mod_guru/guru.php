0906a1478c827dcdc992c8a11952b8e520d33271
herry88 <herry081288@gmail.com>
2021-06-08 16:03:28 +0700

    GuruModul

--- sia/modul/mod_guru/guru.php
+++ sia/modul/mod_guru/guru.php
@@ -6,20 +6,68 @@ if (empty($_SESSION['namauser']) and empty($_SESSION['passuser'])) {
         <br>
         <a href=\"index.php\"><h3>Halaman Login</h3></a>
     </center>";
+} else {
+    $aksi = "modul/mod_guru/aksi_guru.php";
+    $act = isset($_GET['act']) ? $_GET['act'] : '';
+    switch ($act) {
+            //tampil data guru
+        default:
+            echo "
+       <div class=\"content-header\">
+       <div class=\"container-flsudo uid\">
+           <div class=\"row mb-2\">
+               <div class=\"col-sm-6\">
+                   <h1 class=\"m-0\">Halaman Guru</h1>
+               </div><!-- /.col -->
+               <div class=\"col-sm-6\">
+                   <ol class=\"breadcrumb float-sm-right\">
+                       <li class=\"breadcrumb-item\"><a href=\"#\">Home</a></li>
+                       <li class=\"breadcrumb-item active\">Halaman Guru</li>
+                   </ol>
+               </div><!-- /.col -->
+           </div><!-- /.row -->
+       </div><!-- /.container-fluid -->
+   </div>";
+            echo "
+        <div class=\"content\">
+            <div class=\"container-fluid\">
+                <div class=\"row\">
+                    <div class=\"col-lg-8\">
+                        <div class=\"card card-primary card-outline\">
+                            <div class=\"card-header\">
+                               <button type=\"button\" class=\"btn btn-primary\" onclick=window.location.href=\"?module=tahunakademik&act=tambahtahunakademik\">Add Page</button>
+                            </div>
+                            <div class=\"card-body\">
+                                <div class=\"card-title\">
+                                    <table class=\"table table-bordered\">
+                                        <thead>
+                                            <th>no</th>
+                                            <th>Nama Guru</th>
+                                            <th>Alamat</th>
+                                            <th>Status</th>
+                                            <th>Tools</th>
+                                        </thead>";
+
+            echo " <tbody>
+                                            <tr>
+                                                <td>1</td>
+                                                <td>1</td>
+                                                <td>1</td>
+                                                <td>1</td>
+                                                <td></td>
+                                            </tr>";
+
+            echo "
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>";
+
+            break;
+    }
 }
-?>
-<div class="content-header">
-    <div class="container-flsudo uid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Halaman Guru</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Halaman Guru</li>
-                </ol>
-            </div><!-- /.col -->
-        </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
-</div>
