26b33a37388b38acdabb769c08da7de4c5f5483f
herry88 <herry081288@gmail.com>
2021-06-10 16:14:03 +0700

    AddSaveGuru

---
+++ sia/modul/mod_guru/aksi_guru.php
@@ -0,0 +1,23 @@
+<?php
+//function untuk simpan data
+session_start();
+if(empty($_SESSION['namauser']) AND empty($_SESSION['passuser'])){
+    echo "Anda harus login terlebih dahulu, <a href=\"../../index.php\">Login</a>";
+} else{
+    include "../../config/conn.php";
+    $module = $_GET['module'];
+    $act = $_GET['act'];
+    //function save input guru
+    if($module == 'guru' AND $act=='input'){
+        $nm_guru = $_POST['nm_guru'];
+        $alamat = $_POST['alamat'];
+        $status = $_POST['masaaktif'];
+     
+
+        //save data
+        $input = "INSERT INTO guru(nm_guru, alamat, status)VALUES('$nm_guru','$alamat','$status')";
+        $query = mysqli_query($conn, $input);
+        header("location:../../media.php?module=".$module);
+    } 
+}
+?>
--- sia/modul/mod_guru/guru.php
+++ sia/modul/mod_guru/guru.php
@@ -46,16 +46,31 @@ if (empty($_SESSION['namauser']) and empty($_SESSION['passuser'])) {
                                             <th>Alamat</th>
                                             <th>Status</th>
                                             <th>Tools</th>
-                                        </thead>";
+                                        </thead>
+                                        <tbody>";
 
-            echo " <tbody>
-                                            <tr>
-                                                <td>1</td>
-                                                <td>1</td>
-                                                <td>1</td>
-                                                <td>1</td>
-                                                <td></td>
-                                            </tr>";
+            $query = "SELECT * FROM guru";
+            $no = 1;
+            $hasil = mysqli_query($conn, $query);
+            while ($t = mysqli_fetch_array($hasil)) {
+                echo "<tr>
+                                                <td>$no</td>
+                                                <td>$t[nm_guru]</td>
+                                                <td>$t[alamat]</td>";
+                if ($t['status'] == 'Y') {
+                    echo "<td>
+                            <a href=\"#\" class='btn btn-success'>$t[status]</a>
+                        </td>";
+                } else {
+                    echo "<td>
+                            <a href=\"#\" class='btn btn-danger'>$t[status]</a>
+                        </td>";
+                }
+                echo "
+                        <td><a href='?module=guru&act=editguru&id=$t[id_guru]'>Edit</a></td>
+                </tr>";
+                $no++;
+            }
 
             echo "
                                         </tbody>
@@ -97,15 +112,21 @@ if (empty($_SESSION['namauser']) and empty($_SESSION['passuser'])) {
                         <div class=\"card-body\">
                             
                             <div class=\"card-title\">
-                                <form action='$aksi?module=tahunakademik&act=input' method=\"POST\">
+                                <form action='$aksi?module=guru&act=input' method=\"POST\">
                                     <table class='table table-bordered'>
                                             <tr>
-                                                <td>Tahun Akademik :</td>
-                                                <td><input type=\"text\" class=\"form-control\" name=\"tahun_akademik\"></td>
+                                                <td>Nama Guru :</td>
+                                                <td><input type=\"text\" class=\"form-control\" name=\"nm_guru\"></td>
                                             </tr>
                                             <tr>
-                                                <td>Keterangan</td>
-                                                <td><input type=\"text\" name=\"keterangan\" class='form-control'></td>
+                                                <td>Alamat</td>
+                                                <td><input type=\"text\" name=\"alamat\" class='form-control'></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Status:</td>
+                                                <td>
+                                                    <input type =\"radio\" value=\"Y\" name=\"masaaktif\" checked> Aktif <input type =\"radio\" value=\"N\" name=\"masaaktif\"> Tidak Aktif 
+                                                </td>
                                             </tr>
                                             <tr>
                                                 <td>&nbsp;</td>
@@ -122,6 +143,24 @@ if (empty($_SESSION['namauser']) and empty($_SESSION['passuser'])) {
             </div>
         </div>
     </div>";
+            break;
+        case "editguru":
+            echo "
+       <div class=\"content-header\">
+       <div class=\"container-flsudo uid\">
+           <div class=\"row mb-2\">
+               <div class=\"col-sm-6\">
+                   <h1 class=\"m-0\">Edit Guru</h1>
+               </div><!-- /.col -->
+               <div class=\"col-sm-6\">
+                   <ol class=\"breadcrumb float-sm-right\">
+                       <li class=\"breadcrumb-item\"><a href=\"#\">Home</a></li>
+                       <li class=\"breadcrumb-item active\">Halaman Guru</li>
+                   </ol>
+               </div><!-- /.col -->
+           </div><!-- /.row -->
+       </div><!-- /.container-fluid -->
+   </div>";
             break;
     }
 }
